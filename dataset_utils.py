0b1cdab41badb12402642976c21a552dfc99874f
Mouna LABIADH <labiadh.mouna@gmail.com>
2017-12-15 21:49:13 +0100

    Update dataset_utils.py

--- dataset_utils.py
+++ dataset_utils.py
@@ -156,24 +156,6 @@ def TRStoCSV(parent_dir, sub_dirs):
 
 def download_youtube_audio(yid, start, end, label):
     
-    removed_ids =['--GreAs6Iow', '--DpfB2gIlM', '--G9OFlSImU', '--RyKAReM6M', '--aLzScChzY', '--w3fFIHw1g'
-                 , '-0WjFrEBt8Y', '-144GuRT1h8', '-1vLeS9Ix0U', '-2AMSKyGe8w', '-2hDoZ4yUy0', '-2nPnGv5xjc'
-                 , '-2tYgFIroIA']
-    if not ((os.path.isfile(audioset_dir+'/Audio/yvid__'+yid+'__'+str(label)+'.wav')) or
-            (yid == '--DpfB2gIlM') or
-            (yid == '--G9OFlSImU') or
-            (yid == '--GreAs6Iow') or
-            (yid == '--RyKAReM6M') or
-            (yid == '--aLzScChzY') or
-            (yid == '--w3fFIHw1g') or
-            (yid == '-0WjFrEBt8Y') or
-            (yid == '-144GuRT1h8') or
-            (yid == '-1vLeS9Ix0U') or
-            (yid == '-2AMSKyGe8w') or
-            (yid == '-2hDoZ4yUy0') or
-            (yid == '-2nPnGv5xjc') or
-            (yid == '-2tYgFIroIA')):
-
         ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -205,109 +187,6 @@ def download_youtube_audio(yid, start, end, label):
     else:
         print(yid+': Already exists!')
 
-
-def main():
-    print("Shall we start ??...")
-
-    # print("\nReading CHIME dataset ...")
-
-    # sampling_rate = 16000
-    # audio_ext="*.wav" # extension of the audio files (default: *.wav)
-    # label_ext="*.csv" # entension of the labels file (default: *.csv)
-    
-    # sweethome_parole_dir = 'sweethome/corpus-parole-domotique-t2'
-    # sweethome_multimodal_dir = 'sweethome/corpus-multimodal-t2'
-    # sweethome_parole_sub_dirs = ['S00','S01','S02','S03','S04','S05','S06','S07','S08','S09','S10',
-    # 'S11','S12','S13','S14','S16','S17','S18','S19','S20','S21','S22','S23','S24','S25']
-
-    # sweethome_multimodal_sub_dirs = ['S01','S02','S03','S04','S05','S06','S07','S08','S09','S10',
-    # 'S11','S12','S13','S14','S16','S17','S18','S19','S20']
-
-    # transcript_dir = 'transcrit'
-    # transcript_sub_dirs = ['.']
-
-    # bref120_dir = 'BREF120/BREF120_WAV/dev/wav'
-
-    # normalize_wav(wav_dir=transcript_dir, sampling_rate=sampling_rate, audio_ext=audio_ext)
-    # normalize_wav(wav_dir=bref120_dir, sampling_rate=sampling_rate, audio_ext=audio_ext)
-
-    # for l, sub_dir in enumerate(sweethome_parole_sub_dirs):
-    #     sh_dir = os.path.join(sweethome_parole_dir, 'Audio', sub_dir)
-    #     normalize_wav(wav_dir=sh_dir, sampling_rate=sampling_rate, audio_ext=audio_ext)
-
-    # for l, sub_dir in enumerate(sweethome_multimodal_sub_dirs):
-    #     sh_dir = os.path.join(sweethome_multimodal_dir, 'Audio', sub_dir)
-    #     normalize_wav(wav_dir=sh_dir, sampling_rate=sampling_rate, audio_ext=audio_ext)
-    
-    print("\nReading SWEETHOME dataset ...")
-    X_sh=[]
-
-    with open('dataset/X_CHIME_Transcript_nohot.csv', 'r') as f:
-        reader = csv.reader(f)
-        data = list(reader)
-        
-    for l in data:
-        X_sh.append([float(i) for i in l])
-
-    Y_sh=[]   
-
-    with open('dataset/Y_CHIME_Transcript_nohot.csv', 'r') as f:
-        reader = csv.reader(f)
-        data = list(reader)
-
-    for l in data:
-        Y_sh.append([int(float(i)) for i in l])
-
-    X_sh=np.asarray(X_sh)
-    Y_sh=np.asarray(Y_sh)
-
-    # print("Splitting dataset on training/test")
-
-    # stop = len(X_chim)
-    # split = int(stop*0.7)
-
-
-    # X_train, X_test = X_chime[:split,:], X_chime[split:,:]
-    # Y_train, Y_test = Y_chime[:split], Y_chime[split:]
-
-    # sh_linear_train = liblinear_data_format(X_train, Y_train)
-    sh_linear_test = liblinear_data_format(X_sh, Y_sh)
-
-    # print(np.asarray(chime_linear_train))
-    print(np.asarray(sh_linear_test))
-
-    liblinear_dir='dataset/liblinear'   
-
-    # with open(os.path.join(liblinear_dir,'CHIME_dummy_liblinear_train.csv'), 'w') as a:
-    #   wxtrain = csv.writer(a, delimiter =' ')
-    #   wxtrain.writerows(chime_linear_train)
-
-    with open(os.path.join(liblinear_dir,'CHIME_Transcript_liblinear_train.csv'), 'w') as b:
-      wxtest = csv.writer(b, delimiter =' ')
-      wxtest.writerows(sh_linear_test)
-
-
-    print("LIBLINEAR SWEETHOME saved to ",os.path.join(liblinear_dir,'SWEETHOME_liblinear.csv'))
-
-   
-
-    # sweethome_parole_dir = 'sweethome/corpus-parole-domotique-t2'
-    # sweethome_multimodal_dir = 'sweethome/corpus-multimodal-t2'
-
-    # sweethome_parole_sub_dirs = ['S00','S01','S02','S03','S04','S05','S06','S07','S08','S09','S10',
-    # 'S11','S12','S13','S14','S16','S17','S18','S19','S20','S21','S22','S23','S24','S25']
-
-    # sweethome_multimodal_sub_dirs = ['S01','S02','S03','S04','S05','S06','S07','S08','S09','S10',
-    # 'S11','S12','S13','S14','S16','S17','S18','S19','S20']
-
-    # sweethome_parole_sub_dirs = ['Annotations_audio']
-
-    # sweethome_multimodal_sub_dirs = ['Annotations_audio']
-
-    # TRStoCSV(sweethome_parole_dir, sweethome_parole_sub_dirs)
-    # TRStoCSV(sweethome_multimodal_dir, sweethome_multimodal_sub_dirs)
-
-    print("And that's it !...")
 
 if __name__ == "__main__":
     #logger = configure_logging()
